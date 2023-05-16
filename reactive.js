308feb85312a8ece9082b0db0e2181850e957663
victor <xxvnkexin@gmail.com>
2023-05-16 09:53:49 +0800

    feat: more general effect fn collection

--- reactive.js
+++ reactive.js
@@ -1,9 +1,15 @@
 const bucket = new Set()
-
+let activeEffect = null
+const effect = fn => {
+  activeEffect = fn
+  fn()
+}
 const data = {text: 'hello world'}
 const proxyData = new Proxy(data, {
   get(target, key){
-    bucket.add(effect)
+    if(activeEffect){
+      bucket.add(activeEffect)
+    }
     return target[key]
   },
   set(target, key, value){
@@ -13,12 +19,10 @@ const proxyData = new Proxy(data, {
   }
 })
 
-
-const effect = () => {
+effect(() => {
+  console.log('executed');
   document.body.innerHTML = proxyData.text
-}
-
-effect()
+})
 
 setTimeout(() => {
   proxyData.text = 'hello vue3'
